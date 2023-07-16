c20c0ab796e542c77484b02b2d858a773bd0f9ce
Ayush <ayush.kanduri@gmail.com>
2023-07-16 19:23:11 +0530

    Event Listener, State, State Destructuring

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -7,27 +7,45 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default class CartItem extends Component {
+	constructor() {
+		super();
+		this.state = {
+			price: 999,
+			title: "Mobile Phone",
+			qty: 1,
+			img: "",
+		};
+	}
+	increaseQuantity = () => {
+		console.log("this", this.state);
+	};
+	decreaseQuantity = () => {};
+	deleteProduct = () => {};
 	render() {
+		const { title, qty, img, price } = this.state;
 		return (
 			<div className="cart-item">
 				<div className="left-block">
-					<img style={styles.image} src="" alt="" />
+					<img style={styles.image} src={img} alt="" />
 				</div>
 				<div className="right-block">
-					<div style={{ fontSize: 25 }}>Product: Phone</div>
-					<div style={{ color: "#777" }}>Price: Rs.999</div>
-					<div style={{ color: "#777" }}>Quantity: 1</div>
+					<div style={{ fontSize: 25 }}>Product: {title}</div>
+					<div style={{ color: "#777" }}>Price: Rs.{price}</div>
+					<div style={{ color: "#777" }}>Quantity: {qty}</div>
 					<div className="cart-item-actions">
 						<FontAwesomeIcon
 							icon={faCirclePlus}
+							onClick={this.increaseQuantity}
 							className="action-icons"
 						/>
 						<FontAwesomeIcon
 							icon={faCircleMinus}
+							onClick={this.decreaseQuantity}
 							className="action-icons"
 						/>
 						<FontAwesomeIcon
 							icon={faTrashCan}
+							onClick={this.deleteProduct}
 							className="action-icons"
 						/>
 						{/* Buttons */}
