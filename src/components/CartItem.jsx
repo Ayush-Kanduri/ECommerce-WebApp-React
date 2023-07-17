63f6252be7dbf1399bfa5db8de8a29f87af7b18e
Ayush <ayush.kanduri@gmail.com>
2023-07-17 15:50:00 +0530

    Set State in Depth

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -15,14 +15,101 @@ export default class CartItem extends Component {
 			qty: 1,
 			img: "",
 		};
+		this.testing1();
+		this.testing2();
 	}
-	increaseQuantity = () => {
-		console.log("this", this.state);
+	testing1 = () => {
+		// Inside Event Handler Functions //
+		/**
+			@StateObject - It doesn't re-render. // (State {qty: 1})
+			this.state.qty += 10;
+			console.log("State Object", this.state);
+
+			Output:
+			State Object {price: 999, title: "Mobile Phone", qty: 11, img: ""}
+			Render
+		**/
+		/**
+			@Form1 - Re-Renders Only Once, Asynchronous Nature, Value of Last Call Only 
+			this.setState({ qty: this.state.qty + 1 });
+			this.setState({ qty: this.state.qty + 2 });
+			this.setState({ qty: this.state.qty + 3 }); // (1) + 3 = 4 (State {qty: 4})
+			console.log("State Object", this.state);
+			
+			Output:
+			Render
+			State Object {price: 999, title: "Mobile Phone", qty: 1, img: ""}
+			Render
+		 **/
+		/**
+			@Form2 - Re-Renders Only Once, Asynchronous Nature, Value of All Calls Combined
+			this.setState((prev) => { return { qty: prev.qty + 1 }; });
+			this.setState((prev) => { return { qty: prev.qty + 2 }; });
+			this.setState((prev) => { return { qty: prev.qty + 3 }; }); // (1) + 1 + 2 + 3 = 7 (State {qty: 7})
+			console.log("State Object", this.state);
+			
+			Output:
+			Render
+			State Object {price: 999, title: "Mobile Phone", qty: 1, img: ""}
+			Render
+		**/
 	};
-	decreaseQuantity = () => {};
+	testing2 = () => {
+		// Inside Promises & AJAX //
+		/**
+			@Form1 - Re-Renders Only Once, Asynchronous Nature, Value of Last Call Only 
+			const promise = new Promise((resolve, reject) => {
+				setTimeout(() => { resolve("done"); }, 5000);
+			});
+			promise.then(() => {
+				this.setState({ qty: this.state.qty + 10 });
+				this.setState({ qty: this.state.qty + 10 });
+				this.setState({ qty: this.state.qty + 10 }); // (1) + 10 = 11 (State {qty: 11})
+				console.log("State Object", this.state);
+			});
+
+			Output:
+			Render
+			State Object {price: 999, title: "Mobile Phone", qty: 1, img: ""}
+			Render
+		 **/
+		/**
+			@Form2 - Re-Renders Only Once, Asynchronous Nature, Value of All Calls Combined
+			const promise = new Promise((resolve, reject) => {
+				setTimeout(() => {
+					resolve("done");
+				}, 5000);
+			});
+
+			promise.then(() => {
+				this.setState((prev) => { return { qty: prev.qty + 1 }; });
+				this.setState((prev) => { return { qty: prev.qty + 2 }; });
+				this.setState((prev) => { return { qty: prev.qty + 3 }; }); // (1) + 1 + 2 + 3 = 7 (State {qty: 1})
+				console.log("State Object", this.state);
+			});
+			
+			Output:
+			Render
+			State Object {price: 999, title: "Mobile Phone", qty: 1, img: ""}
+			Render
+		**/
+	};
+	increaseQuantity = () => {
+		this.setState((prev) => {
+			return { qty: prev.qty + 1 };
+		});
+	};
+	decreaseQuantity = () => {
+		const { qty } = this.state;
+		if (qty === 0) return;
+		this.setState((prev) => {
+			return { qty: prev.qty - 1 };
+		});
+	};
 	deleteProduct = () => {};
 	render() {
 		const { title, qty, img, price } = this.state;
+		console.log("Render");
 		return (
 			<div className="cart-item">
 				<div className="left-block">
