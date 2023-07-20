cd9766c4544c219438568cad71669dc47c1f5a83
Ayush <ayush.kanduri@gmail.com>
2023-07-20 23:40:44 +0530

    Firebase Configuration Added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_apiKey,
+	authDomain: process.env.REACT_APP_authDomain,
+	projectId: process.env.REACT_APP_projectId,
+	storageBucket: process.env.REACT_APP_storageBucket,
+	messagingSenderId: process.env.REACT_APP_messagingSenderId,
+	appId: process.env.REACT_APP_appId,
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	// <StrictMode>
-	<App />
-	// </StrictMode>
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
